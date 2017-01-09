a8bb32d37ea6b548e650d093f042a3581b2bb7db
Julian Kingman <julian@designman.io>
2017-01-09 11:18:14 -0500

    updated rehydrate reducer action, per https://github.com/rt2zz/redux-persist/issues/189#issuecomment-271208662

--- connectMeteorRedux.js
+++ connectMeteorRedux.js
@@ -2,107 +2,163 @@
  * Created by Julian on 12/30/16.
  */
 import Meteor from 'react-native-meteor';
-import {createStore} from 'redux';
+import {
+    createStore
+} from 'redux';
 import _ from 'lodash';
+import {
+    REHYDRATE
+} from 'redux-persist/constants'
 
-const meteorReduxReducers = (state  = {}, action) => {
-  // console.log(state, action, Meteor.ddp);
-  const {type, collection, id, fields} = action;
-  const docIndex = state? _.findIndex(state[collection], {_id: id}): undefined;
-  switch (type) {
-    case 'ADDED':
-      if (!state[collection]) {
-        return {
-          ...state,
-          [collection]: [
-            Object.assign(fields, {_id: id}),
-          ],
-        };
-      } else if (!_.find(state[collection], {_id: id})) {
-        return {
-          ...state,
-          [collection]: [
-            ...state[collection],
-            Object.assign(fields, {_id: id}),
-          ],
-        };
-      } else if (_.find(state[collection], {_id: id})) {
-        // console.warn(`${id} not added to ${collection}, duplicate found`);
-      }
-      return state;
-    case 'CHANGED':
-      return _.update(state, `${collection}[${docIndex}]`, (val) => {
-        return _.merge(val, fields);
-      });
-    case 'REMOVED':
-      if (docIndex > -1) {
-        // console.log('rm\'d');
-        const updatedCollection = _.filter(state[collection], (doc) => doc._id !== id);
-        return {
-          ...state,
-          [collection]: updatedCollection,
-        };
-      }
-      console.error(`Couldn't remove ${id}, not found in ${collection} collection`);
-      return state;
-    case 'persist/REHYDRATE':
-      if(typeof Meteor.ddp === undefined || Meteor.ddp.status === "disconnected"){
-        return action.payload;
-      }
-    default:
-      return state;
-  }
+const meteorReduxReducers = (state = {}, action) => {
+    // console.log(state, action, Meteor.ddp);
+    const {
+        type,
+        collection,
+        id,
+        fields
+    } = action;
+    const docIndex = state ? _.findIndex(state[collection], {
+        _id: id
+    }) : undefined;
+    switch (type) {
+        case 'ADDED':
+            if (!state[collection]) {
+                return {
+                    ...state,
+                    [collection]: [
+                        Object.assign(fields, {
+                            _id: id
+                        }),
+                    ],
+                };
+            } else if (!_.find(state[collection], {
+                    _id: id
+                })) {
+                return {
+                    ...state,
+                    [collection]: [
+                        ...state[collection],
+                        Object.assign(fields, {
+                            _id: id
+                        }),
+                    ],
+                };
+            } else if (_.find(state[collection], {
+                    _id: id
+                })) {
+                // console.warn(`${id} not added to ${collection}, duplicate found`);
+            }
+            return state;
+        case 'CHANGED':
+            return _.update(state, `${collection}[${docIndex}]`, (val) => {
+                return _.merge(val, fields);
+                p
+            });
+        case 'REMOVED':
+            if (docIndex > -1) {
+                // console.log('rm\'d');
+                const updatedCollection = _.filter(state[collection], (doc) => doc._id !== id);
+                return {
+                    ...state,
+                    [collection]: updatedCollection,
+                };
+            }
+            console.error(`Couldn't remove ${id}, not found in ${collection} collection`);
+            return state;
+        case REHYDRATE:
+            if (action.payload && (typeof Meteor.ddp === undefined || Meteor.ddp.status === "disconnected")) {
+              if (incoming) return {
+                ...state,
+                  ...action.payload
+              };
+            }
+            return state;
+        default:
+            return state;
+    }
 };
 
 const initMeteorRedux = (preloadedState = undefined, enhancer = null) => {
-  // console.log(preloadedState, enhancer);
-  const MeteorStore = createStore(meteorReduxReducers, preloadedState, enhancer);
+    // console.log(preloadedState, enhancer);
+    const MeteorStore = createStore(meteorReduxReducers, preloadedState, enhancer);
 
-  Meteor.waitDdpConnected(() => {
-    // question: do I need to check for disconnection?
-    let connected = true;
-    Meteor.ddp.on('disconnected', () => {
-      connected = false;
+    Meteor.waitDdpConnected(() => {
+        // question: do I need to check for disconnection?
+        let connected = true;
+        Meteor.ddp.on('disconnected', () => {
+            connected = false;
+        });
+        if (connected) {
+            Meteor.ddp.on('removed', (obj) => {
+                const {
+                    collection,
+                    id,
+                    fields
+                } = obj;
+                MeteorStore.dispatch({
+                    type: 'REMOVED',
+                    collection,
+                    id,
+                    fields
+                });
+            });
+            Meteor.ddp.on('changed', (obj) => {
+                const {
+                    collection,
+                    id,
+                    fields
+                } = obj;
+                MeteorStore.dispatch({
+                    type: 'CHANGED',
+                    collection,
+                    id,
+                    fields
+                });
+            });
+            Meteor.ddp.on('added', (obj) => {
+                const {
+                    collection,
+                    id,
+                    fields
+                } = obj;
+                MeteorStore.dispatch({
+                    type: 'ADDED',
+                    collection,
+                    id,
+                    fields
+                });
+            });
+        }
     });
-    if (connected) {
-      Meteor.ddp.on('removed', (obj) => {
-        const {collection, id, fields} = obj;
-        MeteorStore.dispatch({type: 'REMOVED', collection, id, fields});
-      });
-      Meteor.ddp.on('changed', (obj) => {
-        const {collection, id, fields} = obj;
-        MeteorStore.dispatch({type: 'CHANGED', collection, id, fields});
-      });
-      Meteor.ddp.on('added', (obj) => {
-        const {collection, id, fields} = obj;
-        MeteorStore.dispatch({type: 'ADDED', collection, id, fields});
-      });
-    }
-  });
 
-  return MeteorStore;
+    return MeteorStore;
 };
 
 class MeteorStore {
-  constructor(props){
+    constructor(props) {
 
-  }
+    }
 }
 
 const subscribeCached = (name) => {
-  if(Meteor.ddp && Meteor.ddp.status === 'disconnected'){
+    if (Meteor.ddp && Meteor.ddp.status === 'disconnected') {
 
-  }
-  return Meteor.subscribe(name, arguments)
+    }
+    return Meteor.subscribe(name, arguments)
 }
 
 returnCached = (cursor, store, collectionName) => {
-  if(Meteor.ddp && Meteor.ddp.status === 'disconnected'){
-    return store.getState()[collectionName] || [];
-  }
-  return cursor;
+    if (Meteor.ddp && Meteor.ddp.status === 'disconnected') {
+        return store.getState()[collectionName] || [];
+    }
+    return cursor;
 }
 
-export {meteorReduxReducers, subscribeCached, returnCached};
+export {
+    meteorReduxReducers,
+    subscribeCached,
+    returnCached
+};
 export default initMeteorRedux;
 // export default connectMeteorRedux;
